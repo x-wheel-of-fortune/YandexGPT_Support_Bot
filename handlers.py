45bc61790c61dd1dc02a702e212833e7ac99b760
lalkasoska <35616551+lalkasoska@users.noreply.github.com>
2023-08-13 20:43:48 +0700

    Add session timeout

--- handlers.py
+++ handlers.py
@@ -1,5 +1,5 @@
 import os
-
+import datetime
 from aiogram import F, Router, types
 from aiogram.filters import Command
 from aiogram.types import Message, FSInputFile
@@ -17,11 +17,15 @@ import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
 router = Router()
+SESSION_TIMEOUT = datetime.timedelta(minutes=30)
 
+user_last_interaction = {}
 
 @router.message(Command("start"))
 async def start_handler(msg: Message, state: FSMContext):
     await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.menu)
+    user_id = msg.from_user.id
+    user_last_interaction[user_id] = datetime.datetime.now()
     # await state.set_state(Gen.text_response)
 
 # #@router.message()
@@ -45,46 +49,62 @@ async def input_text_prompt(clbck: CallbackQuery, state: FSMContext):
 @router.message(Gen.text_response)
 @flags.chat_action("typing")
 async def text_response(msg: Message, state: FSMContext):
-    if msg.content_type == types.ContentType.VOICE:
-        file_id = msg.voice.file_id
-        file = await bot.get_file(file_id)
-        file_path = file.file_path
-        file_on_disk = Path("", f"{file_id}.tmp")
-        await bot.download_file(file_path, destination=file_on_disk)
-        prompt = await utils.stt(file_on_disk)
-        print(prompt)
-        os.remove(file_on_disk)  # Удаление временного файла
+    user_id = msg.from_user.id
+    if user_id in user_last_interaction and (datetime.datetime.now() - user_last_interaction[user_id]) >= SESSION_TIMEOUT:
+        print("previous session ran out")
+        await start_handler(msg, state)
     else:
-        prompt = msg.text
-    mesg = await msg.answer(text.gen_wait)
-    res = await utils.generate_classified_response(prompt)
-    if not res or not res[0]:
-        return await mesg.edit_text(text.gen_error, reply_markup=kb.iexit_kb)
-    await mesg.edit_text(res[0] + text.text_watermark, disable_web_page_preview=True)
+        if msg.content_type == types.ContentType.VOICE:
+            file_id = msg.voice.file_id
+            file = await bot.get_file(file_id)
+            file_path = file.file_path
+            file_on_disk = Path("", f"{file_id}.tmp")
+            await bot.download_file(file_path, destination=file_on_disk)
+            prompt = await utils.stt(file_on_disk)
+            print(prompt)
+            os.remove(file_on_disk)  # Удаление временного файла
+        else:
+            prompt = msg.text
+        mesg = await msg.answer(text.gen_wait)
+        res = await utils.generate_classified_response(prompt)
+        if not res or not res[0]:
+            return await mesg.edit_text(text.gen_error, reply_markup=kb.iexit_kb)
+        await mesg.edit_text(res[0] + text.text_watermark, disable_web_page_preview=True)
 
+        user_last_interaction[user_id] = datetime.datetime.now()
 
 @router.message(Gen.audio_response)
 @flags.chat_action("typing")
 async def audio_response(msg: Message, state: FSMContext):
-    if msg.content_type == types.ContentType.VOICE:
-        file_id = msg.voice.file_id
-        file = await bot.get_file(file_id)
-        file_path = file.file_path
-        file_on_disk = Path("", f"{file_id}.tmp")
-        await bot.download_file(file_path, destination=file_on_disk)
-        prompt = await utils.stt(file_on_disk)
-        print(prompt)
-        os.remove(file_on_disk)  # Удаление временного файла
-    else:
-        prompt = msg.text
-    mesg = await msg.answer(text.gen_wait)
-    res = await utils.generate_classified_response(prompt)
-    if not res:
-        return await mesg.edit_text(text.gen_error, reply_markup=kb.iexit_kb)
-    out_filename = await utils.tts(res[0])
-    # Отправка голосового сообщения
-    path = Path("", out_filename)
-    voice = FSInputFile(path)
-    await bot.send_voice(msg.from_user.id, voice)
+    user_id = msg.from_user.id
+    if user_id in user_last_interaction and (
+            datetime.datetime.now() - user_last_interaction[
+        user_id]) >= SESSION_TIMEOUT:
+        print("previous session ran out")
 
-    os.remove(out_filename)  # Удаление временного файла
+        await start_handler(msg, state)
+    else:
+        if msg.content_type == types.ContentType.VOICE:
+            file_id = msg.voice.file_id
+            file = await bot.get_file(file_id)
+            file_path = file.file_path
+            file_on_disk = Path("", f"{file_id}.tmp")
+            await bot.download_file(file_path, destination=file_on_disk)
+            prompt = await utils.stt(file_on_disk)
+            print(prompt)
+            os.remove(file_on_disk)  # Удаление временного файла
+        else:
+            prompt = msg.text
+        mesg = await msg.answer(text.gen_wait)
+        res = await utils.generate_classified_response(prompt)
+        if not res:
+            return await mesg.edit_text(text.gen_error, reply_markup=kb.iexit_kb)
+        out_filename = await utils.tts(res[0])
+        # Отправка голосового сообщения
+        path = Path("", out_filename)
+        voice = FSInputFile(path)
+        await bot.send_voice(msg.from_user.id, voice)
+
+        os.remove(out_filename)  # Удаление временного файла
+        user_id = msg.from_user.id
+        user_last_interaction[user_id] = datetime.datetime.now()
