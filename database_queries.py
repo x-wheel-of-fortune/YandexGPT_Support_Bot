05251e0b238fbaa15452acde83f5dd13207c0c55
Sonya Sergeeva <sonya451f@gmail.com>
2023-08-16 23:15:13 +0700

    Refactor database_queries.py

--- database_queries.py
+++ database_queries.py
@@ -1,30 +1,38 @@
 import sqlite3
 
 
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
+
+
 def get_by_id(user_id: int):
-    # Подключение к базе данных
-    conn = sqlite3.connect('resources/users_yandex.db')
-    cursor = conn.cursor()
+    # Use context manager for the database connection
+    with DatabaseConnection('resources/users_yandex.db') as conn:
+        cursor = conn.cursor()
 
-    # Выполнение запроса
-    query = f"SELECT * FROM users_data_yandex WHERE user_id = ? LIMIT 1"
-    cursor.execute(query, (user_id,))
+        query = f"SELECT * FROM users_data_yandex WHERE user_id = ?"
+        cursor.execute(query, (user_id,))
 
-    # Извлечение данных
-    user_data = cursor.fetchone()
+        user_data = cursor.fetchone()
 
-    # Закрытие соединения
-    conn.close()
+        column_names = [description[0] for description in cursor.description]
 
-    column_names = [description[0] for description in cursor.description]
+        if user_data:
+            user_data_dict = dict(zip(column_names, user_data))
+        else:
+            user_data_dict = "У этого пользователя нет активных заказов."
 
-    if user_data:
-        # Создание словаря с данными
-        user_data_dict = dict(zip(column_names, user_data))
-    else:
-        user_data_dict = "У этого пользователя нет активных заказов."
-
-    return user_data_dict
+        return user_data_dict
 
 
 print(get_by_id(7))
