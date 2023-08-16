8a26bd752cce3a45a5194b526924ee60c148630d
Sonya Sergeeva <sonya451f@gmail.com>
2023-08-17 01:35:34 +0700

    Optimize func other_problems()

--- handlers.py
+++ handlers.py
@@ -27,19 +27,6 @@ SESSION_TIMEOUT = datetime.timedelta(minutes=30)
 user_last_interaction = {}
 
 
-#global_constants
-USER_ID = 7
-
-SCENARIOS = {
-    0: other_problems,
-    1: order_late,
-    2: order_damaged,
-    3: order_expired,
-    4: order_wrong,
-}
-
-
-
 @router.message(Command("start"))
 @router.message(StateFilter(None))
 async def start_handler(msg: Message, state: FSMContext):
@@ -48,20 +35,6 @@ async def start_handler(msg: Message, state: FSMContext):
     user_id = 7  # msg.from_user.id
     await state.set_state(Gen.waiting_for_question)
     user_last_interaction[user_id] = datetime.datetime.now()
-
-
-@router.callback_query(F.data == "text_response")
-async def input_text_prompt(clbck: CallbackQuery, state: FSMContext):
-    await state.set_state(Gen.text_response)
-    await clbck.message.answer(const_answers["gen_text"],
-                               reply_markup=keyboard.exit_kb)
-
-
-@router.callback_query(F.data == "audio_response")
-async def input_text_prompt(clbck: CallbackQuery, state: FSMContext):
-    await state.set_state(Gen.audio_response)
-    await clbck.message.answer(const_answers["gen_text"],
-                               reply_markup=keyboard.exit_kb)
 
 
 @router.message(Text(const_answers["case_completed"]))
@@ -81,8 +54,7 @@ async def response(msg: Message, state: FSMContext):
         await start_handler(msg, state)
         return
     prompt = await receive_message(msg)
-    mesg = await msg.answer(const_answers["gen_wait"])
-    # print(await order_wrong_cut(msg, state))
+    await msg.answer(const_answers["gen_wait"])
     problem_type = await utils.classify(prompt)
     await SCENARIOS[problem_type](msg, state)
 
@@ -96,7 +68,6 @@ async def receive_message(message: Message):
     file_on_disk = Path("", f"{file_id}.tmp")
     await bot.download_file(file_path, destination=file_on_disk)
     prompt = await utils.speech.text_to_speech(file_on_disk)
-    # print(prompt)
     os.remove(file_on_disk)  # Удаление временного файла
     return prompt
 
@@ -115,32 +86,28 @@ async def send_message(msg: Message, res, mesg, user_id):
     user_last_interaction[user_id] = datetime.datetime.now()
 
 
+# Common function to handle responses
+async def handle_response(msg: Message, state: FSMContext, instruction_key):
+    instruction = instructions["problem"][0][instruction_key]
+    res = await utils.generate_response("", instruction)
+    await msg.answer(res[0])
+    await state.set_state(Gen.waiting_for_question)
+
 async def other_problems(msg: Message, state: FSMContext):
-    instruction = instructions["problem"][0]["classify"]
-    type = await utils.generate_response(msg.text, instruction)
-    if not type[0] or not type[0].isdigit() or int(type[0]) < 0 or int(
-            type[0]) > 1:
-        prob = 0
+    type = await utils.generate_response(msg.text, instructions["problem"][0]["classify"])
+    if not type[0] or not type[0].isdigit() or int(type[0]) < 0 or int(type[0]) > 1:
+        await handle_response(msg, state, "not_support")
     else:
-        prob = 1
-    if prob == 0:
-        instruction = instructions["problem"][0]["not_support"]
-        res = await utils.generate_response("", instruction)
-        await msg.answer(res[0])
-        await state.set_state(Gen.waiting_for_question)
-    else:
-        instruction = instructions["problem"][0]["support"]
-        res = await utils.generate_response("", instruction)
-        await msg.answer(res[0])
-        # support_response = input()
+        await handle_response(msg, state, "support")
+
 
 
 async def order_late(msg: Message, state: FSMContext):
     user_question = msg.text
     instruction_yes = instructions["problem"][1]["delivery_goes"] + \
-                      instructions["database"] + str(get_by_id(user_id))
+                      instructions["database"] + str(get_by_id(USER_ID))
     instruction_no = instructions["problem"][1]["delivery_failed"] + \
-                     instructions["database"] + str(get_by_id(user_id))
+                     instructions["database"] + str(get_by_id(USER_ID))
     print("Вы задерживаетесь с доставкой. Вы привезете заказ?")
     delivery_response = input("Да/Нет ")
     # delivery_response = utils.generate_support_answer()
@@ -160,7 +127,7 @@ async def order_late(msg: Message, state: FSMContext):
 
 async def order_damaged(msg: Message, state: FSMContext):
     instruction = instructions["base"] + instructions["problem"][3]["base"] + \
-                  instructions["database"] + str(get_by_id(user_id))
+                  instructions["database"] + str(get_by_id(USER_ID))
     res = await utils.generate_response(msg.text, instruction)
     await msg.answer(res[0])  # Отправка ответа пользователю
     await state.set_state(Gen.waiting_for_damaged_photo)
@@ -175,7 +142,7 @@ async def order_damaged_photo(msg: Message, state: FSMContext):
     # support_response = utils.generate_support_answer()
     if support_response == "Да":
         instruction = instructions["base"] + instructions["problem"][2][
-            "damaged"] + instructions["database"] + str(get_by_id(user_id))
+            "damaged"] + instructions["database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
         #    Вставить кнопки в клавиатуру пользователя при нажатии на
         #    которые отправится сообщение
@@ -184,7 +151,7 @@ async def order_damaged_photo(msg: Message, state: FSMContext):
         await state.set_state(Gen.waiting_for_refund_method_damaged)
     else:
         instruction = instructions["base"] + instructions["problem"][2][
-            "not_damaged"] + instructions["database"] + str(get_by_id(user_id))
+            "not_damaged"] + instructions["database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
         await state.set_state(Gen.waiting_for_other_question)
         await msg.answer(res[0])
@@ -199,21 +166,21 @@ async def choosing_refund_method_damaged(msg: Message, state: FSMContext):
         instruction = instructions["base"] + \
                       instructions["selected_answer_damaged"]["product"] + \
                       instructions[
-                          "database"] + str(get_by_id(user_id))
+                          "database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
     elif ans == "Купон":
         instruction = instructions["base"] + \
                       instructions["selected_answer_damaged"]["coupon"] + \
                       instructions[
                           "database"] + str(get_by_id(
-            user_id)) + "Это купон для клиента, предоставь код купона " \
+            USER_ID)) + "Это купон для клиента, предоставь код купона " \
                         "клиенту" + utils.generate_ticket()
         res = await utils.generate_response("", instruction_text=instruction)
     elif ans == "Карта":
         instruction = instructions["base"] + \
                       instructions["selected_answer_damaged"]["card"] + \
                       instructions[
-                          "database"] + str(get_by_id(user_id))
+                          "database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
     else:
         res = "Пожалуйста, повторите Ваш вопрос."
@@ -222,7 +189,7 @@ async def choosing_refund_method_damaged(msg: Message, state: FSMContext):
 
 async def order_expired(msg: Message, state: FSMContext):
     instruction = instructions["base"] + instructions["problem"][3]["base"] + \
-                  instructions["database"] + str(get_by_id(user_id))
+                  instructions["database"] + str(get_by_id(USER_ID))
     res = await utils.generate_response(msg.text, instruction)
     await msg.answer(res[0])  # Отправка ответа пользователю
     await state.set_state(Gen.waiting_for_expired_photo)
@@ -237,11 +204,11 @@ async def order_expired_photo(msg: Message, state: FSMContext):
     support_response = utils.generate_support_answer()
     if support_response == "Да":
         instruction = instructions["base"] + instructions["problem"][3][
-            "expired"] + instructions["database"] + str(get_by_id(user_id))
+            "expired"] + instructions["database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
     else:
         instruction = instructions["base"] + instructions["problem"][3][
-            "not_expired"] + instructions["database"] + str(get_by_id(user_id))
+            "not_expired"] + instructions["database"] + str(get_by_id(USER_ID))
         res = await utils.generate_response("", instruction_text=instruction)
     await msg.answer(res[0])
     await state.set_state(Gen.waiting_for_other_question)
@@ -251,3 +218,14 @@ async def order_wrong(msg: Message, state: FSMContext):
     instruction = instructions["base"] + instructions["problem"][4]["support"]
     res = await utils.generate_response("", instruction)
     await msg.answer(res[0])
+
+#global_constants
+USER_ID = 7
+
+SCENARIOS = {
+    0: other_problems,
+    1: order_late,
+    2: order_damaged,
+    3: order_expired,
+    4: order_wrong,
+}
